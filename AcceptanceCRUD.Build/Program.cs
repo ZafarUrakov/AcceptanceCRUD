cdcb5db5730a5b99be1209db1a5fca6899ac899a
JohnnySenior <jamshidbektursunboev@gmail.com>
2024-05-28 11:16:01 +0900

    CODE RUB: Update .Net SDK

--- AcceptanceCRUD.Build/Program.cs
+++ AcceptanceCRUD.Build/Program.cs
@@ -48,7 +48,7 @@ var githubPipeline = new GithubPipeline
 
                         TargetDotNetVersion = new TargetDotNetVersion
                         {
-                            DotNetVersion = "6.0.100-rc.1.21463.6",
+                            DotNetVersion = "8.0.300",
                             IncludePrerelease = true
                         }
                     },
